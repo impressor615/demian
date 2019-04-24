657bb05c3ed6c04d9936cc9685fb44d00667c4dc
seonghyeon <impressor7@gmail.com>
2019-04-24 14:18:40 +0900

    nextjs-with-styled-component - add next-images

--- nextjs-with-styled-component/next.config.js
+++ nextjs-with-styled-component/next.config.js
@@ -1,6 +1,8 @@
 const withPlugins = require('next-compose-plugins');
+const withImages = require('next-images');
 const withTypescript = require('@zeit/next-typescript');
 
 module.exports = withPlugins([
   withTypescript,
+  withImages,
 ]);
